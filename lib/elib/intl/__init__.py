50d765b06c34b9f848442b24315db9b2b6d1b69f
Dieter Verfaillie <dieterv@optionexplicit.be>
2010-05-02 15:08:29 +0200

    fix copyright notice

--- lib/elib/intl/__init__.py
+++ lib/elib/intl/__init__.py
@@ -2,18 +2,20 @@
 #
 # Copyright © 2007-2010 Dieter Verfaillie <dieterv@optionexplicit.be>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# This file is part of elib.intl.
+#
+# elib.intl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# elib.intl is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with elib.intl. If not, see <http://www.gnu.org/licenses/>.
 
 
 '''
